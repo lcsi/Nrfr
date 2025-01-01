f94a48e80c3f8aed8b3f71b768acec4f3eee6775
Antkites <mayizhuifengzheng@gmail.com>
2025-01-01 20:47:27 +0800

    fix: 版本兼容性问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,10 +10,10 @@ android {
 
     defaultConfig {
         applicationId = "com.github.nrfr"
-        minSdk = 34
+        minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
